425bd9b67829e06b03002d09d8603ecbe08fa230
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-02 15:49:30 +0100

    JSNACK3: creato un array di oggetti

---
+++ jsnack-3/main.js
@@ -0,0 +1,34 @@
+/*  CONSEGNA: A partire da un array di oggetti, creare una copia dell'array
+e aggiungere ai singoli elementi dell'array una nuova proprietà "position" che contiene una lettera casuale.  */
+
+
+$(document).ready(function() {
+
+    // creo un array di oggetti
+    // ciascun oggetto rappresenta un libro
+    var books = [
+        {
+            'title' : 'Frankenstein',
+            'author' : 'Mary Shelley',
+            'genre' : 'Gothic novel'
+        },
+        {
+            'title' : 'Hard times',
+            'author' : 'Charles Dickens',
+            'genre' : 'Novel'
+        },
+        {
+            'title' : 'Fahrenheit 451',
+            'author' : 'Ray Bradbury',
+            'genre' : 'Dystopian'
+        },
+        {
+            'title' : 'Three Men in a Boat',
+            'author' : 'Jerome Klapka Jerome',
+            'genre' : 'Comedy novel'
+        }
+    ];
+
+
+
+});
