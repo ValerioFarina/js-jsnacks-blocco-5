e2d884254797f3404ec1a9227229ddf38d01a6f8
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-02 19:07:03 +0100

    JSNACK2: semplificata la parte dedicata al controllo dell'input utente

--- jsnack-2/main.js
+++ jsnack-2/main.js
@@ -16,24 +16,25 @@ $(document).ready(function() {
     ];
     console.log(names);
 
+    // creo delle variabili in cui salvo la posizione minima e la posizione massima che un elemento dentro l'array names può occupare
+    var minPosition = 0;
+    var maxPosition = names.length - 1;
+
     // chiedo all'utente due numeri
-    // continuo a chiederglieli fino a quando l'input inserito dall'utente
-    // o non è un numero
-    // oppure è un numero non compreso tra 0 e names.length - 1
-    // infatti, ciascun numero inserito dall'utente dovrebbe essere un indice all'interno dell'array names
+    // continuo a chiederglieli fino a quando l'input non è valido (cioè, fino a quando non è un numero compreso tra minPosition e maxPosition)
     do {
-        var userNumber1 = parseInt(prompt('Inserisci un numero compreso tra 0 e ' + (names.length - 1)));
-        if (isNaN(userNumber1) || userNumber1 < 0 || userNumber1 >= names.length) {
+        var userNumber1 = parseInt(prompt('Inserisci un numero compreso tra ' + minPosition + ' e ' + maxPosition));
+        if (!isInputValid(userNumber1, minPosition, maxPosition)) {
             alert('Per favore, inserisci un numero valido');
         }
-    } while (isNaN(userNumber1) || userNumber1 < 0 || userNumber1 >= names.length);
+    } while (!isInputValid(userNumber1, minPosition, maxPosition));
 
     do {
-        var userNumber2 = parseInt(prompt('Inserisci un numero compreso tra 0 e ' + (names.length - 1)));
-        if (isNaN(userNumber2) || userNumber2 < 0 || userNumber2 >= names.length) {
+        var userNumber2 = parseInt(prompt('Inserisci un numero compreso tra ' + minPosition + ' e ' + maxPosition));
+        if (!isInputValid(userNumber2, minPosition, maxPosition)) {
             alert('Per favore, inserisci un numero valido');
         }
-    } while (isNaN(userNumber2) || userNumber2 < 0 || userNumber2 >= names.length);
+    } while (!isInputValid(userNumber2, minPosition, maxPosition));
 
     // calcolo minimo e massimo tra userNumber1 e userNumber2
     var minNumber = Math.min(userNumber1, userNumber2);
@@ -47,3 +48,42 @@ $(document).ready(function() {
 
 
 });
+
+
+
+// ********************* LE MIE FUNZIONI *********************
+// questa funzione prende in input un valore e controlla se è un numero oppure no
+// se è un numero (o comunque una stringa costituita da caratteri numerici), ritorna true
+// altrimenti, ritorna false
+function isNumber(value) {
+    if (!isNaN(value)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+// questa funzione prende in input tre numeri num, min e max, e controlla se num è compreso tra min e max
+// se num è compreso tra min e max (eventualmente uguale a min o max), allora la funzione ritorna true
+// altrimenti ritorna false
+function isInRange(num, min, max) {
+    if (num >= min && num <=max) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+// questa funzione serve a controllare se l'input inserito dall'utente è valido
+// nel nostro caso, l'input dell'utente è valido se entrambe le seguenti condizioni sono soddisfatte:
+    // - l'input è un numero
+    // - l'input è compreso tra un min e un max
+function isInputValid(input, min, max) {
+    if (isNumber(input) && isInRange(input, min, max)) {
+        return true;
+    } else {
+        return false;
+    }
+}
