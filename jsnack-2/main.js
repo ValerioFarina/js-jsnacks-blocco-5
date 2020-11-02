ae8dd94ee591fcd4fe586ea83df3b06a61519542
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-02 15:31:39 +0100

    JSNACK2: creato un nuovo array con i valori che (all'interno dell'array originale dei nomi) si trovano in una posizione compresa tra i due numeri inseriti dall'utente

--- jsnack-2/main.js
+++ jsnack-2/main.js
@@ -17,22 +17,33 @@ $(document).ready(function() {
     console.log(names);
 
     // chiedo all'utente due numeri
+    // continuo a chiederglieli fino a quando l'input inserito dall'utente
+    // o non è un numero
+    // oppure è un numero non compreso tra 0 e names.length - 1
+    // infatti, ciascun numero inserito dall'utente dovrebbe essere un indice all'interno dell'array names
     do {
-        var userNumber1 = parseInt(prompt('Inserisci un numero'));
-        if (isNaN(userNumber1)) {
-            alert('Per favore, inserisci un numero');
+        var userNumber1 = parseInt(prompt('Inserisci un numero compreso tra 0 e ' + (names.length - 1)));
+        if (isNaN(userNumber1) || userNumber1 < 0 || userNumber1 >= names.length) {
+            alert('Per favore, inserisci un numero valido');
         }
-    } while (isNaN(userNumber1));
+    } while (isNaN(userNumber1) || userNumber1 < 0 || userNumber1 >= names.length);
 
     do {
-        var userNumber2 = parseInt(prompt('Inserisci un numero'));
-        if (isNaN(userNumber2)) {
-            alert('Per favore, inserisci un numero');
+        var userNumber2 = parseInt(prompt('Inserisci un numero compreso tra 0 e ' + (names.length - 1)));
+        if (isNaN(userNumber2) || userNumber2 < 0 || userNumber2 >= names.length) {
+            alert('Per favore, inserisci un numero valido');
         }
-    } while (isNaN(userNumber2));
+    } while (isNaN(userNumber2) || userNumber2 < 0 || userNumber2 >= names.length);
 
-    console.log(userNumber1);
-    console.log(userNumber2);
+    // calcolo minimo e massimo tra userNumber1 e userNumber2
+    var minNumber = Math.min(userNumber1, userNumber2);
+    var maxNumber = Math.max(userNumber1, userNumber2);
+
+    // creo un nuovo array con i valori che (all'interno dell'array names) si trovano in una posizione compresa tra minNumber e maxNumber
+    // (minNumber e maxNumber inclusi)
+    var namesSliced = names.slice(minNumber, maxNumber + 1);
+
+    console.log(namesSliced);
 
 
 });
