525bf880d39f6a04776668e31ac1c9b964787401
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-02 14:47:30 +0100

    SNACK1: creato un array di oggetti "biciclette"

---
+++ jsnack-1/main.js
@@ -0,0 +1,34 @@
+/*  CONSEGNA: Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.  */
+
+$(document).ready(function() {
+    // creo un array di oggetti
+    // ogni oggetto corrisponde ad una bicicletta caratterizzata da nome e peso
+    var bicycles = [
+        {
+            'name' : 'Bianchi',
+            'weight' : 20
+        },
+        {
+            'name' : 'Garelli',
+            'weight' : 17
+        },
+        {
+            'name' : 'Olympia',
+            'weight' : 22
+        },
+        {
+            'name' : 'Lapierre',
+            'weight' : 16
+        },
+        {
+            'name' : 'Bickerton',
+            'weight' : 19
+        }
+    ];
+
+    console.log(bicycles);
+
+
+
+});
