bbf32743786095d3f7cd6ff7baf019a913096c82
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-02 14:59:23 +0100

    JSNACK1: determinato attraverso un ciclo for qual è la bici più leggera; stampato a video il risultato

--- jsnack-1/main.js
+++ jsnack-1/main.js
@@ -27,7 +27,20 @@ $(document).ready(function() {
         }
     ];
 
-    console.log(bicycles);
+    // creo una variabile lightestBicycle in cui salverò la bicicletta più leggera
+    // inizialmente, pongo questa variabile uguale alla prima bicicletta presente nell'array bicycles
+    var lightestBicycle = bicycles[0];
+
+    // ciclo sull'array bicycles partendo dal secondo elemento (cioè, da quello con indice 1)
+    for (var i = 1; i < bicycles.length; i++) {
+        // non appena trovo una bici più leggera della bici salvata nella variabile lightestBicycle, sovrascrivo il valore di questa variabile
+        if (bicycles[i].weight < lightestBicycle.weight) {
+            lightestBicycle = bicycles[i];
+        }
+    }
+
+    // stampo la bici più leggera
+    console.log(lightestBicycle);
 
 
 
