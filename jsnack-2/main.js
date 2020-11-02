0e76daf12aecfa30bb3155a24067112012825e6e
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-02 15:04:10 +0100

    JSNACK2: creato un array di nomi

---
+++ jsnack-2/main.js
@@ -0,0 +1,19 @@
+/*  CONSEGNA: Creare un array di nomi e chiedere all'utente due numeri.
+Creare un nuovo array con i valori che si trovano in una posizione compresa tra i due numeri inseriti dall'utente.  */
+
+$(document).ready(function() {
+
+    // creo un array di nomi
+    var names = [
+        'Luca',
+        'Paolo',
+        'Giovanna',
+        'Stefania',
+        'Federico',
+        'Mirco',
+        'Antonio',
+        'Laura'
+    ];
+
+
+});
